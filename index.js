5d30f677d7f750104e6d32e3db737882cdce7a8f
vebradev <justinas@vebra.dev>
2019-07-08 15:14:00 +0300

    Setup express server with simple GET endpoint.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // implement your API here
+const express = require('express');
+const server = express();
+const port = 3005;
+
+const Db = require('./data/db');
+
+server.use(express.json());
+
+// GET
+server.get('/api/users/', (req, res) => {
+    Db.find()
+      .then(data => {
+          res
+          .status(200)
+          .json(data);
+      })
+      .catch(() => {
+          res
+          .status(500)
+          .json({ errorMessage: "Please provide name and bio for the user." });
+      });
+});
+
+// LISTEN
+server.listen(port, () => {
+    console.log(`Listening on ${port}`);
+})
